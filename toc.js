42a2915cfb8a31961c67f731c898b41f65205c73
Mark Wubben <mark@novemberborn.net>
2014-04-15 14:48:02 +0100

    Generated documentation for cb6538781bfb81d34ff95d3b06a639ea3eb8868a

---
+++ toc.js
@@ -0,0 +1,226 @@
+window.files = [
+{
+  "path": "index.html",
+  "originalName": "README.md",
+  "originalPath": "README.md",
+  "name": "index.html",
+  "title": "streamist",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "streamist",
+      "title": "streamist"
+    },
+    {
+      "level": 2,
+      "slug": "installation",
+      "title": "Installation"
+    },
+    {
+      "level": 2,
+      "slug": "usage",
+      "title": "Usage"
+    }
+  ]
+},
+{
+  "path": "lib/main.js.html",
+  "originalName": "main.js",
+  "originalPath": "lib/main.js",
+  "name": "main.js.html",
+  "lang": "javascript",
+  "title": "main",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "main",
+      "title": "main"
+    },
+    {
+      "level": 1,
+      "slug": "streamist",
+      "title": "Streamist"
+    },
+    {
+      "level": 2,
+      "slug": "unreadablestreamerror",
+      "title": "UnreadableStreamError"
+    },
+    {
+      "level": 2,
+      "slug": "endofstreamerror",
+      "title": "EndOfStreamError"
+    }
+  ]
+},
+{
+  "path": "lib/Streamist.js.html",
+  "originalName": "Streamist.js",
+  "originalPath": "lib/Streamist.js",
+  "name": "Streamist.js.html",
+  "lang": "javascript",
+  "title": "Streamist(streamOrThenable)",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "streamist-streamorthenable-",
+      "title": "Streamist(streamOrThenable)"
+    },
+    {
+      "level": 2,
+      "slug": "streamist-stream",
+      "title": "Streamist#stream"
+    },
+    {
+      "level": 2,
+      "slug": "streamist-complete",
+      "title": "Streamist#complete"
+    },
+    {
+      "level": 2,
+      "slug": "streamist-cancel-",
+      "title": "Streamist#cancel()"
+    },
+    {
+      "level": 2,
+      "slug": "generic-consuming-instance-methods",
+      "title": "Generic consuming instance methods"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-consumeobject-size-",
+      "title": "Streamist#consumeObject(size)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-consume-size-",
+      "title": "Streamist#consume(size)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-toarray-constructor-",
+      "title": "Streamist#toArray([constructor])"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-tobuffer-length-",
+      "title": "Streamist#toBuffer([length])"
+    },
+    {
+      "level": 2,
+      "slug": "non-streaming-iterators",
+      "title": "Non-streaming iterators"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-each-iterator-",
+      "title": "Streamist#each(iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-eachparallel-maxconcurrent-iterator-",
+      "title": "Streamist#eachParallel(maxConcurrent, iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-detect-iterator-",
+      "title": "Streamist#detect(iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-detectparallel-maxconcurrent-iterator-",
+      "title": "Streamist#detectParallel(maxConcurrent, iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-some-iterator-",
+      "title": "Streamist#some(iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-someparallel-maxconcurrent-iterator-",
+      "title": "Streamist#someParallel(maxConcurrent, iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-every-iterator-",
+      "title": "Streamist#every(iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-everyparallel-maxconcurrent-iterator-",
+      "title": "Streamist#everyParallel(maxConcurrent, iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-foldl-initialvalue-iterator-",
+      "title": "Streamist#foldl(initialValue, iterator)"
+    },
+    {
+      "level": 2,
+      "slug": "streaming-iterators",
+      "title": "Streaming iterators"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-map-iterator-options-",
+      "title": "Streamist#map(iterator, [options])"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-mapparallel-maxconcurrent-iterator-options-",
+      "title": "Streamist#mapParallel(maxConcurrent, iterator, [options])"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-filter-iterator-",
+      "title": "Streamist#filter(iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-filterparallel-maxconcurrent-iterator-",
+      "title": "Streamist#filterParallel(maxConcurrent, iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-filterout-iterator-",
+      "title": "Streamist#filterOut(iterator)"
+    },
+    {
+      "level": 3,
+      "slug": "streamist-filteroutparallel-maxconcurrent-iterator-",
+      "title": "Streamist#filterOutParallel(maxConcurrent, iterator)"
+    }
+  ]
+},
+{
+  "path": "lib/UnreadableStreamError.js.html",
+  "originalName": "UnreadableStreamError.js",
+  "originalPath": "lib/UnreadableStreamError.js",
+  "name": "UnreadableStreamError.js.html",
+  "lang": "javascript",
+  "title": "UnreadableStreamError",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "unreadablestreamerror",
+      "title": "UnreadableStreamError"
+    }
+  ]
+},
+{
+  "path": "lib/EndOfStreamError.js.html",
+  "originalName": "EndOfStreamError.js",
+  "originalPath": "lib/EndOfStreamError.js",
+  "name": "EndOfStreamError.js.html",
+  "lang": "javascript",
+  "title": "EndOfStreamError",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "endofstreamerror",
+      "title": "EndOfStreamError"
+    }
+  ]
+}
+];
